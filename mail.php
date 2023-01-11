971cd2e726283aae25990db48112033f5b30a8fa
Shahnawaz Alam <snawaza243@gmail.com>
2023-01-11 11:31:00 +0530

    update

--- mail.php
+++ mail.php
@@ -1,19 +1,17 @@
 <?php
-$name = $_POST['name']
-;$email = $_POST['email']
-;$message = $_POST['message'];
+//get data from form  
 
-$to = "snawaza243@gmail.com";
-
-$subject = "Mail from portfolio Demo 1";
-$text = "Name = ".$name. " \r\nEmail = ".$email. "\r\nMessage = ".$message;
-$headers = "From: noreply@indiantechnoera.in"."\r\n"."CC:thisoneissam@gmail.com";
-if($email !=NULL)
-{
-    mail($to,$subject,$text, $headers);
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "youremail@mail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
 }
-
+//redirect
 header("Location:thankyou.html");
-
-
 ?>
